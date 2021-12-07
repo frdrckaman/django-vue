f928b60662a823be19c528ee7d35d6985c4b8b39
frdrckaman <frdrckaman@gmail.com>
2021-12-07 14:30:56 +0300

    permission,viewset & roles

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import users, register, login, AuthenticatedUser, logout
+from .views import users, register, login, AuthenticatedUser, logout, PermissionApiView, RoleViewSet
 
 urlpatterns = [
     path('users/', users),
     path('register', register),
     path('login', login),
     path('logout', logout),
-    path('user', AuthenticatedUser.as_view())
+    path('user', AuthenticatedUser.as_view()),
+    path('permission', PermissionApiView.as_view()),
+    path('roles', RoleViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('roles/<str:pk>', RoleViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    }))
 ]
