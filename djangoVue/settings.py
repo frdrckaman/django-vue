26d91fac42562df2781889111e61e0e5d848b5c9
frdrckaman <frdrckaman@gmail.com>
2021-12-10 14:18:37 +0300

    FIleUpload

--- djangoVue/settings.py
+++ djangoVue/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,3 +122,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.core.files.storage import default_storage
 from rest_framework import mixins, generics
+from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from djangoVue.pagenation import CustomPagination
 from products.models import Product
@@ -37,3 +39,18 @@ class ProductGenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixi
 
     def delete(self, request, pk=None):
         return self.destroy(request, pk)
+
+
+class FileUploadView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    parser_classes = (MultiPartParser, )
+
+    def post(self, request):
+        file = request.FILES['image']
+        file_name = default_storage.save(file.name, file)
+        url = default_storage.url(file_name)
+
+        return Response({
+            "url": "http://localhost:8000/api/media/" + url
+        })
